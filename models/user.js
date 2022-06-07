650fdcf6922b45892ffd9423e71e756936f3962f
Mark Sayong <mbenneth7@yahoo.com>
2022-06-07 12:36:20 -0400

    edit user model for passport integration

--- models/user.js
+++ models/user.js
@@ -1,12 +1,20 @@
 //DETERMINING WHAT WE WANT OUR USER DATA TO CONTAIN
 
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = {
-    username: String,
-    email: String,
-    password: String
-};
+const UserSchema = new Schema({
+    email: {
+        type :String,
+        required: true,
+        unique: true
+    },
+});
+
+//PASSPORT, ADDS ON USERNAME AND PASSWORD TO USER SCHEMA
+UserSchema.plugin(passportLocalMongoose);
+
+
 
 module.exports = mongoose.model('User', UserSchema);
