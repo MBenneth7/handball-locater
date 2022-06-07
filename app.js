c1095b4b05a4950c17febbf293259e3dc1576b28
Mark Sayong <mbenneth7@yahoo.com>
2022-06-07 12:27:22 -0400

    add passport for login authentication

--- app.js
+++ app.js
@@ -138,10 +138,13 @@ app.get('/parks/:id',catchAsync(async(req,res)=>{
     const {id} = req.params;
     //'populate' USED TO SHOW 'reviews' ASSOCIATED W/PARK
     const park = await Park.findById(id).populate('reviews');
+    
+    //REDIRECT IF PARK NOT FOUND
     if(!park){
         req.flash('error', 'Park not found!!!');
         return res.redirect('/parks');
     }
+
     console.log(park);
     res.render('parks/show', {park});
 }));
