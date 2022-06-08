65a11dadc2e1f818869da49d1fde5e3887e04459
Mark Sayong <mbenneth7@yahoo.com>
2022-06-08 16:14:24 -0400

    add map display w/mapbox to individual parks

---
+++ public/scripts/showPageMap.js
@@ -0,0 +1,9 @@
+//'mapToken' DEFINED IN 'show.ejs' FILE
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
