f359430e4c5fd2b5c35f20295d6cc6f28de66741
Mark Sayong <mbenneth7@yahoo.com>
2022-06-16 12:54:52 -0400

    fixed upload to cloudinary to upload into "handball-locator" directory

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -11,8 +11,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
     cloudinary,
-    folder: 'Handball-Locator',
-    allowedFormats: ['jpeg', 'png', 'jpg']
+    params: {
+        folder: 'Handball-Locator',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
 });
 
 module.exports = {
