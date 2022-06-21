073591a07caa59e40a33fbdf078ff167f7e0702a
Mark Sayong <mbenneth7@yahoo.com>
2022-06-21 12:19:13 -0400

    modify joi schemas to prevent cross scripting in input fields, install html sanitize to help

--- utilities/joiSchemas.js
+++ utilities/joiSchemas.js
@@ -1,11 +1,36 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHTML = require('sanitize-html');
+
+//EXTENTSION TO PREVENT CROSS-SCRIPTING, WE SANITIZE INPUTS FROM POTENTIAL JS SCRIPTS
+
+const extension = (joi) =>({
+    type: 'string',
+    base: joi.string(),
+    messages:{
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules:{
+        escapeHTML: {
+            validate(value, helpers){
+                const clean = sanitizeHTML(value,{
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if(clean !== value) return helpers.error('string.escapeHTML', {value})
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 //REQUIRING THE DATA FOR REVIEWS ON THE SERVER SIDE
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required(),
-        body: Joi.string().required()
+        body: Joi.string().required().escapeHTML()
     }).required()
 });
 
@@ -13,6 +38,6 @@ module.exports.reviewSchema = Joi.object({
 
 module.exports.commentSchema = Joi.object({
     comment: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML()
     }).required()
-});
+}); 
